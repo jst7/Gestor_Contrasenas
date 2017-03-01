153fb37500d4aea766b4d71996452820f37ed3af
Jorge Segovia Tormo <Jorge Segovia Tormo>
2017-03-01 11:13:16 +0100

    certificados TLS https

--- servidor.go
+++ servidor.go
@@ -11,9 +11,9 @@ func saludoInicio(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1> Servidor arrancado 9797 </h1>")
 }
 
-func main() {
-	http.HandleFunc("/", saludoInicio)       // define la ruta
-	err := http.ListenAndServe(":9797", nil) //  establece el puerto de escucha
+func main() { //https://localhost/
+	http.HandleFunc("/", saludoInicio)                                     // define la ruta
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil) //  establece el puerto de escucha
 	if err != nil {
 		log.Fatal("Error: ", err)
 	}
