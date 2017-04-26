f56e2e3646abe71af5fdd385608cba8bc1dfcac8
jst7 <jorge.segovia.tormo@gmail.com>
2017-04-26 10:28:32 +0200

    Añadidos metodos AES encriptar desencriptar

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -223,17 +223,14 @@ func jSONtoPeticion(pet []byte) peticion { //desjoson
 	return peticionDescifrado
 }
 
-func encrypt(key []byte, text string) string {
-	// key := []byte(keyText)
-	plaintext := []byte(text)
+func encriptar(datosPlanos []byte, key []byte) string {
+	plaintext := datosPlanos
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err)
 	}
 
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
 	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
 	iv := ciphertext[:aes.BlockSize]
 	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
@@ -243,21 +240,18 @@ func encrypt(key []byte, text string) string {
 	stream := cipher.NewCFBEncrypter(block, iv)
 	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
 
-	// convert to base64
 	return base64.URLEncoding.EncodeToString(ciphertext)
 }
 
-// decrypt from base64 to decrypted string
-func decrypt(key []byte, cryptoText string) string {
-	ciphertext, _ := base64.URLEncoding.DecodeString(cryptoText)
+func desencriptar(datosEncriptados string, key []byte) string {
+
+	ciphertext, _ := base64.URLEncoding.DecodeString(datosEncriptados)
 
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err)
 	}
 
-	// The IV needs to be unique, but not secure. Therefore it's common to
-	// include it at the beginning of the ciphertext.
 	if len(ciphertext) < aes.BlockSize {
 		panic("ciphertext too short")
 	}
@@ -266,7 +260,6 @@ func decrypt(key []byte, cryptoText string) string {
 
 	stream := cipher.NewCFBDecrypter(block, iv)
 
-	// XORKeyStream can work in-place if the two arguments are the same.
 	stream.XORKeyStream(ciphertext, ciphertext)
 
 	return fmt.Sprintf("%s", ciphertext)
